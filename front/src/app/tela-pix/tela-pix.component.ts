74728abe72ee4f6d69ba401bdb84acff3f74a0d1
ReisMarcelo <mrenanreis@gmail.com>
2024-10-08 14:06:10 +0000

    criação da tela pix e estilização

--- front/src/app/tela-pix/tela-pix.component.ts
+++ front/src/app/tela-pix/tela-pix.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+
 
 @Component({
-  selector: 'app-tela-pix',
+  selector: 'app-tela-pix', 
   standalone: true,
   imports: [],
   templateUrl: './tela-pix.component.html',
   styleUrl: './tela-pix.component.css'
 })
 export class TelaPixComponent {
+  constructor(private router:Router){}
 
+  voltar (){
+    this.router.navigate(['/tela-inicial'])
+  }
 }
